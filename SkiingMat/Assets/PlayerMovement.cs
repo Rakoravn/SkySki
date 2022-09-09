96a35fac1cc101ab28189dfed37b2bc36bd507b3
chrisnezz <chrisnezz1@gmail.com>
2022-09-09 11:23:03 +0200

    game sound, VFX, finished up UI

--- SkiingMat/Assets/PlayerMovement.cs
+++ SkiingMat/Assets/PlayerMovement.cs
@@ -18,12 +18,32 @@ public class PlayerMovement : MonoBehaviour {
     public Transform ellenSpine;
     public Transform ellenLeftLeg;
     public Transform ellenRightLeg;
+    public GameObject gameOverlay;
+    public GameObject startOverlay;
+    public GameObject endOverlay;
+    public GameObject failedScreen;
+
+    public GameObject newHighscoreText;
+    public TextMeshProUGUI yourTimeText;
+    public TextMeshProUGUI highscoreText;
+    public AudioSource audioSource;
+    public AudioClip pling;
+
+    public ParticleSystem leftPs;
+    public ParticleSystem rightPs;
+    bool isJumping = false;
+
     enum State { PRE, STARTED, FINISHED };
     State currentState = State.PRE;
 
     float actualMoveDir = 0;
 
     void Start() {
+        rb.isKinematic = true;
+        endOverlay.SetActive(false);
+        startOverlay.SetActive(true);
+        gameOverlay.SetActive(false);
+        failedScreen.SetActive(false);
         SetTimeText(time);
         if (PlayerPrefs.HasKey("HIGHSCORE"))
         {
@@ -36,8 +56,15 @@ public class PlayerMovement : MonoBehaviour {
             SetHighscoreText("0");
             PlayerPrefs.SetString("HIGHSCORE", "0");
         }
-        currentState = State.STARTED;
     }   
+
+    public void StartPresseD()
+    {
+        currentState = State.STARTED;
+        startOverlay.SetActive(false);
+        gameOverlay.SetActive(true);
+        rb.isKinematic = false;
+    }
 
     // Update is called once per frame
     void Update() {
@@ -53,6 +80,7 @@ public class PlayerMovement : MonoBehaviour {
                 SetTimeText(GetTotalTime());
                 break;
             case State.FINISHED:
+                rb.isKinematic = true;
                 break;
             default:
                 break;
@@ -70,12 +98,21 @@ public class PlayerMovement : MonoBehaviour {
         } else {
             turnPercentage = actualMoveDir - (actualMoveDir*2);
         }
-        transform.rotation = Quaternion.Euler(10f, actualMoveDir * 55f, -actualMoveDir * 2f);
+        transform.rotation = Quaternion.Euler(10f, actualMoveDir * 52f, -actualMoveDir * 2f);
         ellenSpine.rotation = Quaternion.Euler(ellenSpine.eulerAngles.x, ellenSpine.eulerAngles.y, (-actualMoveDir * 18f) - 90);
         ellenRightLeg.rotation = Quaternion.Euler(ellenRightLeg.eulerAngles.x, ellenRightLeg.eulerAngles.y, (actualMoveDir * 10f) - 90);
         ellenLeftLeg.rotation = Quaternion.Euler(ellenLeftLeg.eulerAngles.x, ellenLeftLeg.eulerAngles.y, (-actualMoveDir * 10f) + 90);
         rb.AddForce(new Vector3(actualMoveDir * mSpeed, 0, constantStillSpeed * (1.35f - turnPercentage)));
         lastMoveX = actualMoveDir;
+
+        if(turnPercentage > 0.6 && !isJumping)
+        {
+            if (!leftPs.isPlaying)
+            {
+                rightPs.Play();
+                leftPs.Play();
+            }
+        }
         //Debug.Log(actualMoveDir * 20f);
     }
 
@@ -83,21 +120,28 @@ public class PlayerMovement : MonoBehaviour {
     {
         if (collision.gameObject.tag == "Killzone")
         {
-            SceneManager.LoadScene(0);
+            gameOverlay.SetActive(false);
+            failedScreen.SetActive(true);
+            currentState = State.FINISHED;
         }
         else if (collision.gameObject.tag == "Boost")
         {
             rb.AddForce(new Vector3(0f, 0f, 5f), ForceMode.Impulse);
+            StartCoroutine(IsJumping());
+
         }
         else if (collision.gameObject.tag == "Finish")
         {
+            endOverlay.SetActive(true);
+            gameOverlay.SetActive(false);
             currentState = State.FINISHED;
-            Debug.Log(highscore);
             if (GetTotalTime() < highscore || highscore == 0)
             {
                 PlayerPrefs.SetString("HIGHSCORE", GetTotalTime().ToString("F2").Replace(",", "."));
+                newHighscoreText.SetActive(true);
             }
-            SceneManager.LoadScene(0);
+            highscoreText.text = "Best time: " + PlayerPrefs.GetString("HIGHSCORE");
+            yourTimeText.text = "Your time: " + GetTotalTime().ToString("F2").Replace(",", ".");
         }
     }
 
@@ -107,6 +151,7 @@ public class PlayerMovement : MonoBehaviour {
         {
             timeMinus += 1;
             StartCoroutine(changeTimeTextColor());
+            audioSource.PlayOneShot(pling);
             Destroy(other.gameObject);
         }
     }
@@ -115,9 +160,15 @@ public class PlayerMovement : MonoBehaviour {
     {
         timerText.color = Color.green;
         yield return new WaitForSeconds(0.4f);
-        timerText.color = Color.white;
+        timerText.color = Color.black;
     }
 
+    private IEnumerator IsJumping()
+    {
+        isJumping = true;
+        yield return new WaitForSeconds(3.5f);
+        isJumping = false;
+    }
     public void SetTimeText(float newTime)
     {
         timerText.text = "Time: "+newTime.ToString("F2").Replace(",", ".");
@@ -131,5 +182,10 @@ public class PlayerMovement : MonoBehaviour {
     public float  GetTotalTime()
     {
         return time - timeMinus;
+    }
+
+    public void ContinuePresseD()
+    {
+        SceneManager.LoadScene(0);
     }
 }
